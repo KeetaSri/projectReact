514e94bfbcf04c4ff2a0f0abb8adaa2399647d7b
KeetaSri <keetawat.s@gmail.com>
2023-05-25 06:02:04 +0700

    implemented feature 'editable data(not permanent)'

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-const data = [
+let data = [
   { id: 1, name: 'Item 1' },
   { id: 2, name: 'Item 2' },
   { id: 3, name: 'Item 3' }
@@ -14,7 +15,21 @@ app.get('/api/data', (req, res) => {
   res.json(data);
 });
 
-const port = 3000;
+app.put('/api/data/:id', (req, res) => {
+  const itemId = parseInt(req.params.id);
+  const { name } = req.body;
+
+  data = data.map(item => {
+    if (item.id === itemId) {
+      return { ...item, name };
+    }
+    return item;
+  });
+
+  res.json({ message: 'Item updated successfully' });
+});
+
+const port = 5000;
 app.listen(port, () => {
   console.log(`Backend API server is running on http://localhost:${port}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,71 @@ import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [data, setData] = useState([]);
+  const [editedItem, setEditedItem] = useState('');
 
   useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
     fetch('http://localhost:5000/api/data')
       .then(response => response.json())
       .then(data => setData(data))
       .catch(error => console.log(error));
-  }, []);
+  };
+
+  const handleInputChange = (event, id) => {
+    const updatedData = data.map(item => {
+      if (item.id === id) {
+        return { ...item, name: event.target.value };
+      }
+      return item;
+    });
+    setData(updatedData);
+  };
+
+  const handleEditItem = (id) => {
+    setEditedItem(id);
+  };
+
+  const handleSaveItem = (id) => {
+    const updatedItem = data.find(item => item.id === id);
+    if (updatedItem && updatedItem.name.trim() !== '') {
+      fetch(`http://localhost:5000/api/data/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: updatedItem.name.trim() }),
+      })
+        .then(response => response.json())
+        .then(() => {
+          setEditedItem('');
+          fetchData();
+        })
+        .catch(error => console.log(error));
+    }
+  };
 
   return (
     <div>
       <h1>Frontend Web App</h1>
       <ul>
         {data.map(item => (
-          <li key={item.id}>{item.name}</li>
+          <li key={item.id}>
+            <input
+              type="text"
+              value={item.id === editedItem ? item.name : item.name}
+              readOnly={item.id !== editedItem}
+              onChange={event => handleInputChange(event, item.id)}
+            />
+            {item.id !== editedItem && (
+              <button onClick={() => handleEditItem(item.id)}>Edit</button>
+            )}
+            {item.id === editedItem && (
+              <button onClick={() => handleSaveItem(item.id)}>Save</button>
+            )}
+          </li>
         ))}
       </ul>
     </div>
